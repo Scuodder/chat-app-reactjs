d57a3bcf83834210406dad0c2ba36ae0ccbe901c
Scuodder <sudarshan.sudarshanthakur@gmail.com>
2019-03-26 00:12:42 +0530

    done with all the react setup and component files

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Chatkit from '@pusher/chatkit-client'; 
+import { tokenUrl, instanceLocator } from './config.js' ;
+import './App.css';
+
+class App extends React.Component{
+
+  constructor() {
+    super()
+    this.state = {
+      messages: [] 
+    }
+  }
+
+  componentDidMount() {
+      const chatManager = new Chatkit.ChatManager({
+          instanceLocator, 
+          userId: "Sudarshan",
+          tokenProvider: new Chatkit.TokenProvider({
+              url: tokenUrl 
+          }) 
+      }) 
+
+      chatManager.connect()
+          .then((currentUser) => {
+              currentUser.subscribeToRoom({
+                  roomId: "19389849",
+                  hooks: {
+                      onMessage: (message) => {
+                          console.log('message.text ', message.text) 
+                          this.setState({
+                            messages: [...this.state.messages, message]  
+                          })
+                      }
+                  } 
+              })
+          })
+          .catch((err) => {
+            console.log(err) ;
+          })
+
+
+
+  }
+
+  render() {
+      console.log(this.state.messages)
+      return (
+          <div className ="app">
+
+          </div>
+      )
+  }
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const tokenUrl = 
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/e0d2449f-56a6-41e0-8335-e380bfcc6fc8/token";
+
+const instanceLocator = "v1:us1:e0d2449f-56a6-41e0-8335-e380bfcc6fc8" ;
+
+
+export { tokenUrl, instanceLocator } ;
