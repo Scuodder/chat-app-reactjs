e3a1e93c433797d868d9d71f1f5738249fcc75c2
Scuodder <sudarshan.sudarshanthakur@gmail.com>
2019-03-27 04:44:23 +0530

    Updates

---
+++ precache-manifest.a267699b65866d5249156bd13358874e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f3733beb1ead3918340",
+    "url": "/chat-app-reactjs/static/js/runtime~main.b857abc1.js"
+  },
+  {
+    "revision": "520c8e676a72a60a50a6",
+    "url": "/chat-app-reactjs/static/js/main.9ab46eff.chunk.js"
+  },
+  {
+    "revision": "6e1d79178e52fcd48062",
+    "url": "/chat-app-reactjs/static/js/2.67eba385.chunk.js"
+  },
+  {
+    "revision": "520c8e676a72a60a50a6",
+    "url": "/chat-app-reactjs/static/css/main.2b7ae6fd.chunk.css"
+  },
+  {
+    "revision": "97e106f56ffd1b298ad209cec8ddff6c",
+    "url": "/chat-app-reactjs/index.html"
+  }
+];
